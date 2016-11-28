bd865e7cf1ad4c122939424a7710ca79c10d8e08
Andrew Dilks <Dilks44@gmail.com>
2016-11-28 13:02:29 -0500

    Starting to create trip creation process

--- app/src/main/java/com/schooldev/group8/travelbuddy/MainActivity.java
+++ app/src/main/java/com/schooldev/group8/travelbuddy/MainActivity.java
@@ -1,23 +1,41 @@
 package com.schooldev.group8.travelbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button mCreateTrip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mCreateTrip = (Button) findViewById(R.id.createTrip);
+        mCreateTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(MainActivity.this, CreateTrip.class);
+                startActivity(intent);
+
+            }
+        });
+
         String test = "This is a tesst";
 
 
         String test2 = "omg is this working??";
 
 
-        TextView testTextView = (TextView) findViewById(R.id.testTextBox);
+        //TextView testTextView = (TextView) findViewById(R.id.testTextBox);
 
-        testTextView.setText(test2);
+        //testTextView.setText(test2);
     }
 }
