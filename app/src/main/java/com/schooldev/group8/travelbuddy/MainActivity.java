3f86be98b6f474b5ff4a6a29220bc80072d69066
Andrew Dilks <Dilks44@gmail.com>
2016-11-28 12:08:11 -0500

    test3

---
+++ app/src/main/java/com/schooldev/group8/travelbuddy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.schooldev.group8.travelbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String test = "This is a tesst";
+    }
+}
