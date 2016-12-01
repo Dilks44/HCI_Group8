7b83dd72a58a20553e82930a0125d89054b8f9f1
Andrew Dilks <Dilks44@gmail.com>
2016-11-30 22:52:37 -0500

    starting to add location addition basic functionality

--- app/src/main/java/com/schooldev/group8/travelbuddy/CreateTrip.java
+++ app/src/main/java/com/schooldev/group8/travelbuddy/CreateTrip.java
@@ -1,13 +1,51 @@
 package com.schooldev.group8.travelbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateTrip extends AppCompatActivity {
+
+    private Button mNewItenBtn;
+    private int RESULT_WITH_DATA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trip);
+
+        mNewItenBtn = (Button) findViewById(R.id.addNewItin);
+        mNewItenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(CreateTrip.this, itinerary_details.class);
+                startActivityForResult(intent, RESULT_WITH_DATA);
+
+            }
+
+
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Make sure the request was successful
+        if (resultCode == RESULT_OK) {
+
+            // Check which request we're responding to
+            if (requestCode == RESULT_WITH_DATA) {
+                // The user is returning info
+                // The Intent's data is where we should look for info like location name etc.
+
+                // Do something with the data we have collected.. Specifically add it to the list of
+                // current itinerary items
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/schooldev/group8/travelbuddy/itinerary_details.java
@@ -0,0 +1,17 @@
+package com.schooldev.group8.travelbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class itinerary_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itinerary_details);
+
+
+
+
+    }
+}
