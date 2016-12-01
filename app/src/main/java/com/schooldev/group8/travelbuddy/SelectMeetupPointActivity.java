1119da1756712452681907ff8d42608fd240c14c
Mjb117 <mjb117@terpmail.umd.edu>
2016-11-30 22:21:56 -0500

    Add clicking on map brings up check/cancel toolbar
    Clicking on the map brings up the toolbar.
    Clicking the check brings up a toast message.
    Clicking the x returns to map.
    Made main screens buttons larger.

--- app/src/main/java/com/schooldev/group8/travelbuddy/SelectMeetupPointActivity.java
+++ app/src/main/java/com/schooldev/group8/travelbuddy/SelectMeetupPointActivity.java
@@ -1,7 +1,11 @@
 package com.schooldev.group8.travelbuddy;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class SelectMeetupPointActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class SelectMeetupPointActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_meetup_point);
+    }
+
+    public void onClickCancel (View v) {
+        GridLayout meetup_toolbar = (GridLayout) findViewById(R.id.meetup_toolbar_layout);
+
+        meetup_toolbar.setVisibility(View.GONE);
+    }
+
+    public void onClickCheck (View v) {
+        GridLayout meetup_toolbar = (GridLayout) findViewById(R.id.meetup_toolbar_layout);
+
+        Toast.makeText(this, "Meetup Location Set", Toast.LENGTH_SHORT).show();
+
+        meetup_toolbar.setVisibility(View.GONE);
     }
 }
