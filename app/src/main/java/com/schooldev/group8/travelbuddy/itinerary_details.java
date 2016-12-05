08df886d55b95ec3ad2559cc11eeb536ceb54b09
Andrew Dilks <Dilks44@gmail.com>
2016-12-05 18:48:11 -0500

    added populate with maps btn

--- app/src/main/java/com/schooldev/group8/travelbuddy/itinerary_details.java
+++ app/src/main/java/com/schooldev/group8/travelbuddy/itinerary_details.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class itinerary_details extends AppCompatActivity {
 
     private Button mDoneBtn;
+    private Button mPopulateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,27 @@ public class itinerary_details extends AppCompatActivity {
                 //Intent intent = new Intent(itinerary_details.this, CreateTrip.class);
                 //startActivity(intent);
                 finish();
+
+            }
+        });
+
+        mPopulateBtn = (Button) findViewById(R.id.launchMapsBtn);
+        mPopulateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView locName = (TextView) findViewById(R.id.locationNameData);
+                locName.setText("Eiffel Tower");
+
+                TextView address = (TextView) findViewById(R.id.addressData);
+                address.setText("Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France");
+                TextView startHrs = (TextView) findViewById(R.id.startHours);
+                startHrs.setText("8am");
+                TextView endHrs = (TextView) findViewById(R.id.endHours);
+                endHrs.setText("8pm");
+
+
+
 
             }
         });
