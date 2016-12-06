dd0bb8b012c865c8513c156989b740aede1d4dc7
CMSC434-sarah <sarahchen46@gmail.com>
2016-12-06 00:53:57 -0500

    updating adding members
    grabbing from searchview to make new members

--- app/src/main/java/com/schooldev/group8/travelbuddy/AddMembersActivity.java
+++ app/src/main/java/com/schooldev/group8/travelbuddy/AddMembersActivity.java
@@ -3,18 +3,21 @@ package com.schooldev.group8.travelbuddy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class AddMembersActivity extends AppCompatActivity {
     public final static String ADDED_MEMBERS = "com.example.myfirstapp.ADDED_MEMBERS";
     private ImageButton mBackBtn;
     private Button tempBackBtn;
     private Button tempForwardBtn;
-    private String[] addedMembers;
+    private ArrayList<String> addedMembers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +63,23 @@ public class AddMembersActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(AddMembersActivity.this, MembersActivity.class);
+
+                String toAdd = ((SearchView)findViewById(R.id.searchViewFB)).getQuery().toString().trim();
+                if(!toAdd.isEmpty() && !toAdd.equals("Search"))
+                    addedMembers.add(toAdd);
+                toAdd =((SearchView)findViewById(R.id.searchViewSMS)).getQuery().toString().trim();
+                if(!toAdd.isEmpty() && !toAdd.equals("Search"))
+                    addedMembers.add(toAdd);
+                toAdd =((SearchView)findViewById(R.id.searchViewEmail)).getQuery().toString().trim();
+                if(!toAdd.isEmpty() && !toAdd.equals("Search"))
+                    addedMembers.add(toAdd);
+
+                int numMembers = addedMembers.size();
+                String[] arrayAddedMembers = new String[numMembers];
+                for (int i=0; i<numMembers; i++) {
+                    arrayAddedMembers[i] = addedMembers.get(i);
+                }
+
                 intent.putExtra(ADDED_MEMBERS, addedMembers);
                 startActivity(intent);
             }
