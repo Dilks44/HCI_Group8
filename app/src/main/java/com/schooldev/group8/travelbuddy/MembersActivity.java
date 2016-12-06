9dc07d3d7681582798991ba80513c3bc357a4520
Judy Fan <jfan@godaddy.com>
2016-12-06 00:40:45 -0500

    Finished delete menue

--- app/src/main/java/com/schooldev/group8/travelbuddy/MembersActivity.java
+++ app/src/main/java/com/schooldev/group8/travelbuddy/MembersActivity.java
@@ -1,9 +1,13 @@
 package com.schooldev.group8.travelbuddy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.res.TypedArrayUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -13,26 +17,32 @@ import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TimerTask;
 
 public class MembersActivity extends AppCompatActivity {
     private ArrayAdapter<String> _arrayAdapter;
-    private String[] _memberList;
+    private static ArrayList<String> _memberList;
     private int RESULT_WITH_DATA = 1;
     private Animation slideInRight;
     private Animation slideOutRight;
     private boolean menuActive = false;
     private LinearLayout iconSet;
+    private int _activeMember = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_members);
 
         // Set up animations on the little icon menu
         iconSet = (LinearLayout) findViewById(R.id.iconSet);
@@ -40,31 +50,20 @@ public class MembersActivity extends AppCompatActivity {
         slideOutRight = AnimationUtils.loadAnimation(this, R.anim.slideoutright);
 
         //get past members
-        if (savedInstanceState != null) {
-            _memberList = savedInstanceState.getStringArray("Members");
-        } else {
-            _memberList = new String[]{"me", "jen"};
+        if (_memberList == null) {
+            _memberList = new ArrayList<String>();
+            _memberList.add("Me");
+            _memberList.add("Jen");
         }
 
         //add any new members
         Intent intent = getIntent();
         String[] newMembers = intent.getStringArrayExtra(AddMembersActivity.ADDED_MEMBERS);
 
-        if (newMembers != null) {
-            String[] allAdded = new String[_memberList.length + newMembers.length];
-            int i = 0;
-
-            for (i = 0; i < _memberList.length; i++) {
-                allAdded[i] = _memberList[i];
-            }
-
-            for (int j = 0; j < newMembers.length; j++) {
-                allAdded[i] = newMembers[j];
-                i++;
-            }
+        if(newMembers != null) {
+            _memberList.addAll(Arrays.asList(newMembers));
         }
 
-        setContentView(R.layout.activity_members);
         ListView listView = (ListView) findViewById(R.id.memberList);
 
         _arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, _memberList);
@@ -79,10 +78,57 @@ public class MembersActivity extends AppCompatActivity {
                     iconSet.startAnimation(slideOutRight);
                     iconSet.setVisibility(View.INVISIBLE);
                 }
-
-                iconSet.setTop(v.getTop());
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) iconSet.getLayoutParams();
+                params.height = v.getHeight();
+                params.topMargin=v.getTop();
+                iconSet.setLayoutParams(params);
                 iconSet.setVisibility(View.VISIBLE);
                 iconSet.startAnimation(slideInRight);
+                _activeMember=position;
+            }
+        });
+
+        ImageView backIcon = (ImageView) findViewById(R.id.backIcon);
+        ImageView phoneIcon = (ImageView) findViewById(R.id.phoneIcon);
+        ImageView messageIcon = (ImageView) findViewById(R.id.messageIcon);
+        ImageView deleteIcon = (ImageView) findViewById(R.id.deleteIcon);
+
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iconSet.startAnimation(slideOutRight);
+                iconSet.setVisibility(View.INVISIBLE);
+                menuActive=false;
+            }
+        });
+
+        deleteIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(MembersActivity.this);
+                adb.setTitle("Delete?");
+                adb.setMessage("Are you sure you want to delete " + _memberList.get(_activeMember));
+                adb.setNegativeButton("Cancel", null);
+                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        _memberList.remove(_activeMember);
+                        _arrayAdapter.notifyDataSetChanged();
+                    }});
+                adb.show();
+
+                iconSet.startAnimation(slideOutRight);
+                iconSet.setVisibility(View.INVISIBLE);
+                menuActive=false;
+            }
+        });
+
+        iconSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iconSet.startAnimation(slideOutRight);
+                iconSet.setVisibility(View.INVISIBLE);
+                menuActive=false;
             }
         });
 
@@ -92,18 +138,27 @@ public class MembersActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent= new Intent(MembersActivity.this, AddMembersActivity.class);
-                startActivityForResult(intent, RESULT_WITH_DATA);
+                startActivity(intent);
+            }
+        });
+
+        Button doneBtn = (Button) findViewById(R.id.nextBtn);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MembersActivity.this, MapScreen.class);
+                startActivity(intent);
             }
         });
     }
 
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        // Save the user's current game state
-        savedInstanceState.putStringArray("Members", _memberList);
-
-        // Always call the superclass so it can save the view hierarchy state
-        super.onSaveInstanceState(savedInstanceState);
-    }
+//    public void onSaveInstanceState(Bundle savedInstanceState) {
+//        // Save the user's current game state
+//        savedInstanceState.putStringArrayList("Members", _memberList);
+//
+//        // Always call the superclass so it can save the view hierarchy state
+//        super.onSaveInstanceState(savedInstanceState);
+//    }
 }
 
 //class AnimationTask extends TimerTask {
