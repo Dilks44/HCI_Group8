80021428ad8dd577cf2c472d7bc7cf7eafdcf09d
Andrew Dilks <Dilks44@gmail.com>
2016-12-11 15:53:44 -0500

    added Date Picker functionality to Create Trip view

--- app/src/main/java/com/schooldev/group8/travelbuddy/CreateTrip.java
+++ app/src/main/java/com/schooldev/group8/travelbuddy/CreateTrip.java
@@ -1,12 +1,17 @@
 package com.schooldev.group8.travelbuddy;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class CreateTrip extends AppCompatActivity {
 
@@ -21,6 +26,18 @@ public class CreateTrip extends AppCompatActivity {
     private TextView mLocAddr;
     private boolean isReturned;
 
+    private TextView mStartDateTV;
+    private TextView mEndDateTV;
+    static final int DATE_DIALOG_ID = 0;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    static final int DATE_DIALOG_ID2 = 1;
+    private int mYear2;
+    private int mMonth2;
+    private int mDay2;
+    private boolean secondRound = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +47,20 @@ public class CreateTrip extends AppCompatActivity {
         mLocName = (TextView) findViewById(R.id.locNameTextView);
         mLocTime = (TextView) findViewById(R.id.tripTimeTextView);
         mLocAddr = (TextView) findViewById(R.id.tripAddressView);
+
+        mStartDateTV = (TextView) findViewById(R.id.startDateTV);
+        mEndDateTV = (TextView) findViewById(R.id.endDateTV);
+
+        mStartDateTV.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+        mEndDateTV.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID2);
+            }
+        });
 
 
         toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar1);
@@ -49,6 +80,14 @@ public class CreateTrip extends AppCompatActivity {
 
         });
 
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        mYear2 = c.get(Calendar.YEAR);
+        mMonth2 = c.get(Calendar.MONTH);
+        mDay2 = c.get(Calendar.DAY_OF_MONTH);
+
 
 
 
@@ -64,6 +103,57 @@ public class CreateTrip extends AppCompatActivity {
 
 
     }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            updateDisplay();
+        }
+    };
+    private DatePickerDialog.OnDateSetListener mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            mYear2 = year;
+            mMonth2 = monthOfYear;
+            mDay2 = dayOfMonth;
+            updateDisplay();
+        }
+    };
+
+    private void updateDisplay() {
+        mStartDateTV.setText(new StringBuilder()
+                // Month is 0 based so add 1
+                .append(mMonth + 1).append("-").append(mDay).append("-")
+                .append(mYear).append(" "));
+
+        if (secondRound) {
+            mEndDateTV.setText(new StringBuilder()
+                    // Month is 0 based so add 1
+                    .append(mMonth2 + 1).append("-").append(mDay2).append("-")
+                    .append(mYear2).append(" "));
+        }
+        secondRound = true;
+    }
+
+    // Create and return DatePickerDialog
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,
+                        mDay);
+            case DATE_DIALOG_ID2:
+                return new DatePickerDialog(this, mDateSetListener2, mYear2, mMonth2,
+                        mDay2);
+        }
+        return null;
+    }
+
 
     @Override
     protected void onResume() {
